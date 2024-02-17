3e7951282c810d1f7295700ce96309bc18a91157
ntrllog <ntrllog@gmail.com>
2024-02-17 00:09:22 -0800

    use | as delimiter

--- generate_md.py
+++ generate_md.py
@@ -1,7 +1,7 @@
 import csv, os
 
 with open('comics.txt') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter='\\')
+    csv_reader = csv.reader(csv_file, delimiter='|')
     for row in csv_reader:
         with open(os.path.join('_comics', f'{row[0]}.md'), 'wb') as md_file:
             md_file.write('---\nlayout: comic\ntitle: Random Tidbits | {1}\ncomic_id: {0}\ncomic_title: {1}\n---\n\n## {1}\n\n<img id="img{0}" src="/assets/images/{0}.png">\n\n{2}\n'.format(row[0], row[1], row[2]).encode('utf-8'))
