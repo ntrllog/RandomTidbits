3ded7e220651228a435bd420ee56bd11b36addec
ntrllog <ntrllog@gmail.com>
2024-06-30 15:19:34 -0700

    fix generate script to handle gifs

--- generate_md.py
+++ generate_md.py
@@ -1,7 +1,15 @@
 import csv, os
 
+gifs = [56]
+
+i = 0
 with open('comics.txt') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter='|')
     for row in csv_reader:
+        i += 1
         with open(os.path.join('_comics', f'{row[0]}.md'), 'wb') as md_file:
-            md_file.write('---\nlayout: comic\ntitle: Random Tidbits | {1}\ncomic_id: {0}\ncomic_title: {1}\n---\n\n## {1}\n\n<img id="img{0}" src="/assets/images/{0}.png">\n\n{2}\n'.format(row[0], row[1], row[2]).encode('utf-8'))
+            if i in gifs:
+                extension = "gif"
+            else:
+                extension = "png"
+            md_file.write('---\nlayout: comic\ntitle: Random Tidbits | {1}\ncomic_id: {0}\ncomic_title: {1}\n---\n\n## {1}\n\n<img id="img{0}" src="/assets/images/{0}.{3}">\n\n{2}\n'.format(row[0], row[1], row[2], extension).encode('utf-8'))
