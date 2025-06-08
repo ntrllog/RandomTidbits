a1905a81ee8cdca1429545bfee978be1d0eab31e
ntrllog <ntrllog@gmail.com>
2025-06-08 14:39:29 -0700

    add img-fluid class to script

--- add_new_comic.py
+++ add_new_comic.py
@@ -13,7 +13,7 @@ with open('_redirects', 'w') as redirects_file:
     redirects_file.write(f'/comics /comics/{comic_id}')
 
 with open(os.path.join('_comics', f'{comic_id}.md'), 'wb') as md_file:
-    md_file.write('---\nlayout: comic\ntitle: Random Tidbits | {1}\ncomic_id: {0}\ncomic_title: {1}\n---\n\n## {1}\n\n<img id="img{0}" src="/assets/images/{0}.png">\n\n{2}\n'.format(comic_id, comic_title, title_text).encode('utf-8'))
+    md_file.write('---\nlayout: comic\ntitle: Random Tidbits | {1}\ncomic_id: {0}\ncomic_title: {1}\n---\n\n## {1}\n\n<img id="img{0}" class="img-fluid" src="/assets/images/{0}.png">\n\n{2}\n'.format(comic_id, comic_title, title_text).encode('utf-8'))
 
 with open('comics.txt', 'a') as db:
     db.write(f'\n{comic_id}|{comic_title}|{title_text}')
